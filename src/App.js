70e651ba80d53128ecc062ac90f54a45525bbf49
Segun <lawalsegun72@gmail.com>
2022-11-28 06:24:48 +0100

    feat: Axios get advice from API

--- src/App.js
+++ src/App.js
@@ -9,22 +9,36 @@ import {
 import { ReactComponent as PatternDivider } from "./assets/pattern-divider-desktop.svg";
 import { ReactComponent as MobilePatternDivider } from "./assets/pattern-divider-mobile.svg";
 import { ReactComponent as IconDice } from "./assets/icon-dice.svg";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [advice, setAdvice] = useState();
+  const [adviceNumber, setAdviceNumber] = useState();
+  const [newadvice, setNewAdvice] = useState(false);
+
+  useEffect(() => {
+    axios.get("https://api.adviceslip.com/advice").then(function (response) {
+      setAdvice(response.data.slip.advice);
+      setAdviceNumber(response.data.slip.id);
+    });
+  }, [newadvice]);
+
+  function handleClick() {
+    setNewAdvice((old) => !old);
+  }
+
   return (
     <AppBox className="App">
       <AdviceBox>
-        <AdviceNo>ADVICE #117</AdviceNo>
-        <Advice>
-          "The boy that went home isn't the boy that came back. Gibberish
-          placeholder advice from yours truly. Next level waffle."
-        </Advice>
+        <AdviceNo>ADVICE #{adviceNumber}</AdviceNo>
+        <Advice>{advice}</Advice>
         <Dividers>
           <PatternDivider />
           <MobilePatternDivider />
         </Dividers>
         <DiceBox>
-          <IconDice />
+          <IconDice onClick={handleClick} />
         </DiceBox>
       </AdviceBox>
     </AppBox>
